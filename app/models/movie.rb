f24fa81f951f4c1ccf774d91cd2e4239fa0b991b
mohit-bestpeers <mobodkhe@bestpeers.com>
2023-06-19 10:51:28 +0530

    Added rating functionality based on review

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,4 +3,10 @@ class Movie < ApplicationRecord
   has_many :users, through: :reviews , dependent: :destroy
   belongs_to :user 
   has_one_attached :image
+
+  def update_average_rating
+    self.rating = reviews.average(:star)
+    save
+  end
+
 end
